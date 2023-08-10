e9ddba4a7918d4a1b322abedf6bee7db32cfe307
Thomaz, Lucian (ESI) <lucian.thomaz@adp.com>
2023-08-09 21:04:12 -0300

    Refactor code to move snake content to its own class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,33 +8,15 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0,0), (-20,0), (-40,0)]
 
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-screen.update()
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.2)
+    snake.move()
 
-    # In a snake with 3 segments, for example, it makes the third segment goes to the position of the second segment,
-    # the second segment goes to the position of the first segment. This way, when the head turns to another direction
-    # the tail will be able to follow the head
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
 
 
 
---
+++ snake.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        # In a snake with 3 segments, for example, it makes the third segment goes to the position of the second
+        # segment, the second segment goes to the position of the first segment. This way, when the head turns to
+        # another direction the tail will be able to follow the head
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        self.segments[0].forward(MOVE_DISTANCE)
