6d03b04905858a987fff5a868bff0a76f1b35e64
Thomaz, Lucian (ESI) <lucian.thomaz@adp.com>
2023-08-11 19:50:51 -0300

    Detect colision with tail

--- snake.py
+++ snake.py
@@ -17,11 +17,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         # In a snake with 3 segments, for example, it makes the third segment goes to the position of the second
