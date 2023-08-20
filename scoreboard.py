e9ef0a90ffdb4086931adf2e63c401982281b692
Thomaz, Lucian (ESI) <lucian.thomaz@adp.com>
2023-08-20 17:41:36 -0300

    Writing high score to a file

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = self.read_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -16,13 +17,24 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score(self.score)
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def read_high_score(self):
+        with open("data.txt") as data:
+            return int(data.read())
+
+    def write_high_score(self, score):
+        with open("data.txt", mode="w") as data:
+            data.write(f"{score}")
